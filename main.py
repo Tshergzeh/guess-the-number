4ff0a07097fbc60ea06b75178768c260696940bb
Oluwasegun Ige <40037490+Tshergzeh@users.noreply.github.com>
2022-06-13 17:31:56 +0100

    Added a computer_guess function to invert the game

--- main.py
+++ main.py
@@ -12,4 +12,25 @@ def guess(x):
 
     print("Yay, congrats. You guessed correctly!!!")
 
-guess(100)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+
+        feedback = input(f'Is {guess} too high(H), too low (L) or correct (C)? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print('Yay! I guessed your number correctly!!!')
+
+# Call if user is guessing computer's number, replace 100 with maximum guess range
+# guess(100)
+# Call if computer is guessing user's number, replace 100 with maximum guess range
+# computer_guess(100)
